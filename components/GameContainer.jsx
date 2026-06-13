9e285f4573addac5d6ad7f704489e9d99ab351ec
Darsh Wala <darshwala@gmail.com>
2026-06-13 09:54:04 +0530

    created a function to display game status

--- components/GameContainer.jsx
+++ components/GameContainer.jsx
@@ -96,66 +96,29 @@ export default function GameContainer() {
   ));
 //-------missing word finish -----------------------------------------------------//
 
-let classForStatus = "default";
-let statusHeading = "Welcome!";
-let statusPara = "Hope you're smart enough"
+  function getGameStatus() {
+    if (isGameWon) {
+      return { classForStatus: "game-won", statusHeading: "Congratulations!", statusPara: "You're not as dumb as I thought" };
+    }
+    if (isGameLost) {
+      return { classForStatus: "game-lost", statusHeading: "I told you!!", statusPara: "You better start learning Assembly" };
+    }
 
-if(wrongGuessesCount === 1){
-  classForStatus = "one-wrong"
-  statusHeading = "First Strike"
-  statusPara = "You can do without HTML can't you"
-}
+    const statuses = [
+      { classForStatus: "default", statusHeading: "Welcome!", statusPara: "Hope you're smart enough" },
+      { classForStatus: "one-wrong", statusHeading: "First Strike", statusPara: "You can do without HTML can't you" },
+      { classForStatus: "two-wrong", statusHeading: "Heyy! Think", statusPara: "Farewell CSS and HTML🫡" },
+      { classForStatus: "three-wrong", statusHeading: "Ouch!!", statusPara: "There go the basics of web dev" },
+      { classForStatus: "four-wrong", statusHeading: "Getting dangerous", statusPara: "You're halfway, if you even realise that." },
+      { classForStatus: "five-wrong", statusHeading: "You should quit", statusPara: "Your language pool is shrinking fast." },
+      { classForStatus: "six-wrong", statusHeading: "Close call", statusPara: "Almost out of luck!" },
+      { classForStatus: "seven-wrong", statusHeading: "Should've quit when I told you", statusPara: "One more wrong guess and it's over." },
+    ];
 
-if(wrongGuessesCount === 2){
-  classForStatus = "two-wrong"
-  statusHeading = "Heyy! Think"
-  statusPara = "Farewell CSS and HTML🫡"
-}
+    return statuses[wrongGuessesCount] || statuses[0];
+  }
 
-if(wrongGuessesCount === 3){
-  classForStatus = "three-wrong"
-  statusHeading = "Ouch!!"
-  statusPara = "There go the basics of web dev"
-}
-
-if(wrongGuessesCount === 4){
-  classForStatus = "four-wrong"
-  statusHeading = "Getting dangerous"
-  statusPara = "You're halfway, if you even realise that."
-}
-
-if(wrongGuessesCount === 5){
-  classForStatus = "five-wrong"
-  statusHeading = "You should quit"
-  statusPara = "Your language pool is shrinking fast."
-}
-
-if(wrongGuessesCount === 6){
-  classForStatus = "six-wrong"
-  statusHeading = "Close call"
-  statusPara = "Almost out of luck!"
-}
-
-if(wrongGuessesCount === 7){
-  classForStatus = "seven-wrong"
-  statusHeading = "Should've quit when I told you"
-  statusPara = "One more wrong guess and it's over."
-}
-
-
-
-
-if(isGameWon){
-  classForStatus = "game-won"
-  statusHeading = "Congratulations!"
-  statusPara = "You're not as dumb as I thought"
-}
-
-if(isGameLost){
-  classForStatus = "game-lost"
-  statusHeading = "I told you!!"
-  statusPara = "You better start learning Assembly"
-}
+  const { classForStatus, statusHeading, statusPara } = getGameStatus();
 
 //-----game container return--
 
